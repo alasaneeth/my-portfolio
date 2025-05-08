a27851d3401e00e39ba45b3601844de4e057b8e1
alasaneeth <alasaneeth22@gmail.com>
2025-05-08 20:33:22 +0530

    Change to tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
 import { useState } from 'react'
-//import './App.css'
-import Button from 'react-bootstrap/Button';
-import Header from './Pages/SideBar';
-
 
 
 function App() {
-  const [count, setCount] = useState(0)
-//Terminal Commit on fork
 
   return (
     <div>
-      <Header/>
+      Hellow world
     </div>
   )
 }
