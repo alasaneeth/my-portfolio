938e48b758ddf91316177a65cda21322358941b8
alasaneeth <alasaneeth22@gmail.com>
2025-05-08 19:53:11 +0530

    Designed Footer Page

---
+++ src/Pages/Footer.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer id="footer" className="footer position-relative light-background">
+
+    <div className="container">
+      <div className="copyright text-center ">
+        <p>© <span>Copyright</span> <span> - 2025</span></p>
+      </div>
+      <div className="credits">
+        Designed & Distributed  by <a href="">ALA.SANEETH</a>  
+      </div>
+    </div>
+    
+    </footer>
+  )
+}
+
+export default Footer
+
