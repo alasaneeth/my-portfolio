a6e1030d914536154302fd59c82698cfa9f839df
alasaneeth <alasaneeth22@gmail.com>
2025-05-10 17:04:26 +0530

    Impement Projects page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Header from './components/Pages/Header'
 import "./App.css"
 import Hero from './components/Pages/Hero'
 import About from './components/Pages/About'
+import Projects from './components/Pages/Projects'
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Header/>
       <Hero/>
       <About/>
+      <Projects/>
     </div>
   )
 }
